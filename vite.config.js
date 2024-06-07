3ff5527740459ee05ececee415eeed41c9d44435
nevis676 <110441473+nevis676@users.noreply.github.com>
2024-06-07 14:10:18 +1200

    change origin

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://nevis676.github.io/assignment-one-gh-deployment/"
+  base: "/assignment-one-gh-deployment/"
 })
